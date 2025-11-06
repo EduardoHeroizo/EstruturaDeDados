a567c411714ab3878e5f258967e69352db88334b
EduardoHeroizo <eduardoheroizo1@hotmail.com>
2025-11-06 15:12:57 -0300

    PosicoesElemento

---
+++ PosicoesDeElemento/Solucao.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Solucao{ 
+
+	public static void main(String[] args){ 
+
+		Scanner sc = new Scanner(System.in); 
+		int num = sc.nextInt(); 
+		sc.nextLine(); 
+
+		String[] linha = sc.nextLine().split(" "); 
+		int[] numeros = new int[linha.length]; 
+		
+		for (int i = 0; i < linha.length; i++){
+
+			numeros[i] = Integer.parseInt(linha[i]); 
+
+		} 
+
+		PosicoesElemento(num, numeros); 
+	
+	}
+
+	public static void PosicoesElemento(int num, int[] numeros){
+		
+		String resultado = "";  
+		
+		for (int i = 0; i < numeros.length; i++){
+
+			if (num == numeros[i]){
+
+				resultado = resultado + i + " ";  
+
+			}
+		}
+
+		if (!resultado.equals("")){
+			System.out.println(resultado.trim()); 
+		} else { 
+			System.out.println(-1); 
+		}
+	}
+}
