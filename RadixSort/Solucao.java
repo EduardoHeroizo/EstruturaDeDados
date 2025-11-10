2e3535ceb428e77cdc23565c81491830d5384e8a
EduardoHeroizo <eduardoheroizo1@hotmail.com>
2025-11-10 17:00:17 -0300

    2

---
+++ RadixSort/Solucao.java
@@ -0,0 +1,18 @@
+public class Solucao{
+
+	public static void main(String[] args){
+
+	
+
+	}
+
+	public static void countingSort(int[] v){
+
+
+	}
+
+	public static int maximoValor(int[] v){ 
+
+
+	}
+}
---
+++ TabelaFrequencia/Solucao.java
@@ -0,0 +1,44 @@
+import java.util.Arrays; 
+import java.util.Scanner; 
+public class Solucao{ 
+	
+	public static void main(String[] args){
+
+		Scanner sc = new Scanner(System.in);
+		String[] linha = sc.nextLine().split(" ");
+		int[] nums = new int[linha.length];
+
+		for (int i = 0; i < linha.length;i++){
+			
+			nums[i] = Integer.parseInt(linha[i]);
+
+}
+			tabelaFrequencia(nums);
+
+}
+	public static void tabelaFrequencia(int[] v){ 
+		int[] helper = new int[maiorValor(v) + 1];	
+		String resultado = "";
+		for (int num : v){
+			helper[num] = helper[num] + 1; 
+			}
+		for (int i = 0; i<v.length;i++){
+			if (helper[v[i]] != 0){
+				resultado = resultado + v[i] + "," + helper[v[i]] + " ";
+				helper[v[i]] = 0; 
+			}
+		}
+		System.out.println(resultado.trim());
+}
+
+	public static int maiorValor(int[] v){
+			
+		int maiorValor = v[0];
+		for (int i = 0; i < v.length; i++){ 
+			if (v[i] > maiorValor){
+			maiorValor = v[i];
+		}
+}	
+		return maiorValor;
+	}
+}
